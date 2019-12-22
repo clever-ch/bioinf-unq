bc84ba130710bf355f200f53d711edbaf64ca25f
Uriel Espinoza <urielintemperie@gmail.com>
2019-12-22 07:40:45 -0300

    BLAST caso identidad 100% y caso identidad >40%; descarga PDB

--- TP_Final_MutaViz/main.py
+++ TP_Final_MutaViz/main.py
@@ -1,20 +1,35 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna, generic_rna
+from Bio.Blast import NCBIWWW
+from Bio.PDB import PDBList
+
 #Dependencias para GUI
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 #Dependencia para abortar/terminar programa
 import sys
 
+#BLAST
+from Bio.Blast import NCBIXML
+
+#Dependencias para descargar PDB
+import urllib.request
+import shutil
+
+#Pymol
+#import pymol
+#from pymol import cmd
+
 #Constantes
-CODONES_STOP = ('UAA', 'UAG', 'UGA')
+CODONS_STOP = ('UAA', 'UAG', 'UGA')
+CODON_START = ['AUG']
 proteinaLetras = {'E','F','I','J','L','O','P','Q','Z'}
 
 
-proteina = "ACCGGUUUAACUUGE"
-ARN = "ACCGGUUUAACUUGA"
-ADN = "ACCGGTTTAACTTGA"
+#proteina = "ACCGGUUUAACUUGE"
+#ARN = "ACCGGUUUAACUUGA"
+#ADN = "ACCGGTTTAACTTGA"
 
 def isProtein(fasta):
     letrasUnicas = set(fasta)
@@ -39,33 +54,120 @@ def isARN(fasta):
 
 #Elimina los guines de la secuencia si es que los hubiese
 def desalinear(secuencia):
-    secuencia._set_seq(secuencia.seq.ungap('-')) #TODO medio raro porque tecnicamente un seqRecord es inmutable
+    secuencia._set_seq(secuencia.seq.ungap('-'))
+
+def abortarYMensaje(mensaje):
+    print(mensaje)
+    print('Fin del programa')
+    sys.exit(0)
 
 def divisiblePor3(secuencia):
     print(f'Longitud de la secuencia: {len(secuencia)}')
     if len(secuencia) % 3 != 0:
-        print("La secuencia no es divisible por 3")
-        sys.exit(0)
+        abortarYMensaje("La secuencia no es divisible por 3")
+
+
+def countCodon(secuencia, codonesBuscados):
+    codonesEncontrados = 0
+    for i in range(0, len(secuencia), 3):
+        if secuencia[i:i + 3] in codonesBuscados:
+            codonesEncontrados += 1
+            print(codonesBuscados)
+            print(f'ubicacion: {i}')
+            print(secuencia[i:i + 3])
+    return codonesEncontrados
 
 def hayCodonesStop(secuencia):
     print(secuencia)
-    #isStopCodon = lambda x: any(x[i:i + 3] in CODONES_STOP for i in range(0, len(x), 3))
 
-    codonesEncontrados = 0
-    for i in range(0, len(secuencia), 3):
-        if secuencia.seq[i:i+3] in CODONES_STOP:
-            codonesEncontrados += 1
+    codonesEncontrados = countCodon(secuencia, CODONS_STOP)
 
     if codonesEncontrados > 1:
-        print(f'Hay multiples({codonesEncontrados}) codones de stop en la secuencia')
-    elif codonesEncontrados == 1 and secuencia.seq.endswith(CODONES_STOP):
-        print('Hay un codon de stop pero esta al final asi que no pasa nada')
+        abortarYMensaje(f'Hay multiples({codonesEncontrados}) codones de stop en la secuencia')
+    elif codonesEncontrados == 1 and secuencia.endswith(CODONS_STOP):
+        print('Hay un codon de stop pero esta al final asi que esta bien')
     else:
-        print('No hay codones de stop')
+        abortarYMensaje('No hay codones de stop')
 
+def hayCodonStart(secuencia):
+
+    codonesEncontrados = countCodon(secuencia, CODON_START)
+
+    if codonesEncontrados == 0:
+        print('No hay condon de inicio')
+    else:
+        print('Hay codon de Inicio')
+
+
+def identidadPorcenaje(align):
+    hsp = align.hsps[0]
+    return (hsp.identities / hsp.align_length)*100
+
+def eValue(align):
+    return align.hsps[0].expect
+
+def score(align):
+    return align.hsps[0].score
+
+def bestAlign(aligns):
+    '''
+   for alignment in blast_record.alignments:
+       print("****align****")
+       print("IDENTIDAD: ", identidadPorcenaje(alignment))
+       for hsp in alignment.hsps:
+       #if hsp.expect < E_VALUE_THRESH:
+           print("****Alignment****")
+
+           print("sequence:", alignment.title)
+
+           print("identity:", hsp.identities)
+
+           print("length:", alignment.length)
+
+           print("e value:", hsp.expect)
+
+           print(hsp.query[0:75] + "...")
+
+           print(hsp.match[0:75] + "...")
+
+           print(hsp.sbjct[0:75] + "...")
+   '''
+
+    #Se ordena segun los siguientes criterios:
+    #pordentaje de identidad (decreciente), e value (creciente), score (decreciente)
+    ordenado = sorted(aligns, key=lambda a: (identidadPorcenaje(a), -eValue(a), score(a)), reverse=True)
+
+    '''
+    for alignment in ordenado:
+        print("****align****")
+        print("sequence:", alignment.title)
+        print("IDENTIDAD: ", identidadPorcenaje(alignment))
+        print("e value:", alignment.hsps[0].expect)
+        print("score: ", alignment.hsps[0].score)
+    '''
+
+    best = ordenado[0]
+
+    if identidadPorcenaje(best) < 40:
+        abortarYMensaje("No se ha encontrado secuencia homologa")
+
+
+    return best
+
+
+def pdbId(align):
+    title = align.title
+    endCode = title.find(" ")
+    code = title[0:endCode]
+    pdbId = code.split('|')[3]
+    print("codigo")
+    print(code)
+    print(pdbId)
+    return pdbId
 
 
 if __name__ == '__main__':
+
     root = tk.Tk()
     root.withdraw()
 
@@ -95,15 +197,64 @@ if __name__ == '__main__':
     else:
         seq_selected = secuencias[0]
 
-    print(seq_selected.id)
     desalinear(seq_selected)
+    if isProtein(seq_selected.seq):
+        print("La secuencia es una proteina")
+        sys.exit(0)
+    elif isADN(seq_selected.seq):
+        print("La secuencia era ADN")
+        seq_selected = seq_selected.seq.transcribe()
+    elif isARN(seq_selected.seq):
+        print("La secuencia era ARN")
+        seq_selected = seq_selected.seq
+
+
+    print(seq_selected)
     quiereMarcoLectura = input("¿Desea definir un marco de lectura? Y/N")
     if quiereMarcoLectura == 'Y':
         marcoLectura = (int(input("Inicio: ")), int(input("Final: ")))
         seq_selected = seq_selected[marcoLectura[0]:marcoLectura[1]]
         print("Seccion seleccionada:")
-        print(seq_selected.seq)
+        print(seq_selected)
+
     #Chequeos de la secuencia:
     divisiblePor3(seq_selected)
     hayCodonesStop(seq_selected)
+    hayCodonStart(seq_selected)
     print(seq_selected)
+
+    #BLAST
+    result_handle = NCBIWWW.qblast("blastx", "pdb", seq_selected)
+    with open("my_blast.xml", "w") as save_file:
+        blast_results = result_handle.read()
+        save_file.write(blast_results)
+
+    result_handle.close()
+
+    result_handle = open("my_blast.xml")
+    blast_record = NCBIXML.read(result_handle)
+
+    best = bestAlign(blast_record.alignments)
+
+    print("sequence:", best.title)
+    print("IDENTIDAD: ", identidadPorcenaje(best))
+    print("e value:", best.hsps[0].expect)
+    print("score: ", best.hsps[0].score)
+
+    '''
+    url = "files.rcsb.org/download/3LEE.pdb"
+    file_name = "3LEE.pdb"
+    
+    
+    # Download the file from `url` and save it locally under `file_name`:
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+    '''
+
+    bestPDBId = pdbId(best)
+
+
+    #urllib.request.urlretrieve(url, file_name)
+    pdbl = PDBList()
+    pdbl.retrieve_pdb_file(bestPDBId, file_format='pdb' ,pdir='PDB')
+    print(pdbl)
